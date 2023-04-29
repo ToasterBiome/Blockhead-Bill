341ebd4098834949c6fc4b18addb85b40a5d977c
Byemoh <baiomurang@gmail.com>
2023-04-29 02:35:56 -0500

    end of the first night

--- Character.gd
+++ Character.gd
@@ -3,11 +3,15 @@ extends CharacterBody2D
 var walk_speed: float = 256
 @onready var sprite: AnimatedSprite2D = $Sprite2D
 var held_item: RigidBody2D
+var touched_box: RigidBody2D
+@onready var grabArea: Area2D = $Area2D
 
-func _init():
+func _ready():
 	Global.player = self
+	grabArea.connect("body_entered", Callable(self, "_on_area_entered"))
+	grabArea.connect("body_exited", Callable(self, "_on_area_exited"))
 
-func _process(delta):
+func _process(_delta):
 	velocity = Vector2.ZERO
 	if(Input.get_action_strength("left")):
 		velocity += Vector2.LEFT
@@ -22,7 +26,6 @@ func _process(delta):
 	if(velocity != Vector2.ZERO && sprite.animation != "walk"):
 		sprite.animation = "walk"
 		sprite.play()
-		print("setting")
 	if(velocity == Vector2.ZERO && sprite.animation != "idle"):
 		sprite.animation = "idle"
 		sprite.play()
@@ -32,19 +35,19 @@ func _process(delta):
 	move_and_slide()
 	
 	if(Input.is_action_just_pressed("mouse_left")):
-		if(Global.hovered_item != null):
-			var dist = Global.hovered_item.global_position.distance_to(global_position)
-			if(dist <= 96):
-				pickup_item(Global.hovered_item)
+		if(held_item == null && touched_box != null):
+			pickup_item(touched_box)
 		else:
 			drop_item()
+			
+	
 	
 func pickup_item(item: RigidBody2D):
 	held_item = item
 	held_item.get_parent().remove_child(held_item)
 	add_child(held_item)
 	held_item.on_pickup()
-	Global.hovered_item = null
+	touched_box = null
 	
 func drop_item():
 	if(held_item == null):
@@ -53,6 +56,20 @@ func drop_item():
 	get_tree().get_root().add_child(held_item)
 	var mouse_pos = get_global_mouse_position()
 	var direction: Vector2 = position.direction_to(mouse_pos).snapped(Vector2.ONE)
-	held_item.on_drop(global_position + (direction * 64))
+	sprite.flip_h = (direction.x > 0)
+	held_item.on_drop(global_position + (direction * 96))
 	held_item = null
+	
+func _on_area_entered(body: Node2D):
+	if(touched_box != null):
+		touched_box.modulate = Color.WHITE
+	print("touched: " + str(body.name))
+	touched_box = body
+	touched_box.modulate = Color.GREEN
+	pass
+	
+func _on_area_exited(body: Node2D):
+	if(body == touched_box):
+		touched_box.modulate = Color.WHITE
+		touched_box = null
 	
